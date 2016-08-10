d7a45be27189cdfef8354240f12c42014a84c62e
Marius Jasaitis <marius@marius.dev>
2016-08-10 08:00:35 +0300

    Use dynamic Heroku port

--- server.js
+++ server.js
@@ -20,5 +20,6 @@ var router = express.Router();
 require("./routes/routes")( app, router );
 
 // Start the server
-app.listen(3000);
-console.log('Node server running on port 3000' );
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env );
+});
